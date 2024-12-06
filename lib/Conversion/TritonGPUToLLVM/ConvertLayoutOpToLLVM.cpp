ad9d89116699f94f23da6abf74ea829f905d2bd6
Bert Maher <bertrand@fb.com>
2024-12-05 19:38:29 -0800

    use legacy mma conversion

--- lib/Conversion/TritonGPUToLLVM/ConvertLayoutOpToLLVM.cpp
+++ lib/Conversion/TritonGPUToLLVM/ConvertLayoutOpToLLVM.cpp
@@ -27,7 +27,7 @@ using namespace mlir::triton::gpu;
 
 // XXX(Keren): A temporary knob to control the use of legacy MMA conversion
 // because LinearLayout seems to have some performance issues.
-constexpr bool useLegacyMMAConversion = false;
+constexpr bool useLegacyMMAConversion = true;
 
 struct ConvertLayoutOpConversion
     : public ConvertOpToLLVMPattern<ConvertLayoutOp> {
